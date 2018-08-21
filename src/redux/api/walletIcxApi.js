fddbf0761289980e41f84bdadcdc0dc2ad0a28ef
couldseeme <couldseeme@nomadconnection.com>
2018-08-21 13:09:56 +0900

    Fix payable bug issue

--- src/redux/api/walletIcxApi.js
+++ src/redux/api/walletIcxApi.js
@@ -83,7 +83,6 @@ export function icx_sendTokenApi(privKey, data) {
         "_value": window.web3.toHex(sendAmount)
       }
     })
-    delete newData.value;
     const rawTx = makeIcxRawTx(true, newData);
     const rawTxSigned = signRawTx(privKey, rawTx)
     const result = await icx_sendTransaction(rawTxSigned);

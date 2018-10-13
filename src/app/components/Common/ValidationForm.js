934c75f4509918a5faf1aa1361a5a543a772cc86
spl3en <spl3en.contact@gmail.com>
2018-10-13 18:01:24 +0200

    Update the password validation regexp

--- src/app/components/Common/ValidationForm.js
+++ src/app/components/Common/ValidationForm.js
@@ -113,7 +113,7 @@ class ValidationForm extends Component {
           } else if(this.state.pw.length < 8) {
             pwError = I18n.error.pwErrorEight
             break;
-          } else if(!(/^(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&*()_+{}:<>?]).{8,}$/.test(this.state.pw))) {
+          } else if(!(/^(?=.*\d)(?=.*[a-zA-Z])(?=.*[?!:\.,%+-/*<>{}\(\)\[\]`"'~_^\\|@#$&]).{8,}$/.test(this.state.pw))) {
             pwError = I18n.error.pwErrorMix
             break;
           }  else if((/(.)\1\1/.test(this.state.pw))) {

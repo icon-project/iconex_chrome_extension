7b8f04bd23c91c518c47feb69131173617454daf
Joris Vial <vialjoris@hotmail.com>
2021-08-24 17:29:41 +0900

    Inverted score addresses by mistake

--- src/constants/icxContract.js
+++ src/constants/icxContract.js
@@ -2,13 +2,13 @@ import { getCurrentServer } from 'constants/config.js'
 
 let icxContractData = [
   {
-    "address": "cx0000000000000000000000000000000000000000",
+    "address": "cx0000000000000000000000000000000000000001",
     "name": "GOV"
   }
 ];
 
 if (getCurrentServer('icx') == 'sejong') {
-  icxContractData[0].address = "cx0000000000000000000000000000000000000001";
+  icxContractData[0].address = "cx0000000000000000000000000000000000000000";
 }
 
 export const icxContract = icxContractData
--- src/redux/api/walletIcxApi.js
+++ src/redux/api/walletIcxApi.js
@@ -6,9 +6,9 @@ import BigNumber from 'bignumber.js';
 import { ICX_WALLET_SERVER, ICX_TRACKER_SERVER } from 'constants/config.js';
 import { getCurrentServer } from 'constants/config.js'
 
-let GOVERNANCE_ADDRESS = 'cx0000000000000000000000000000000000000000'
+let GOVERNANCE_ADDRESS = 'cx0000000000000000000000000000000000000001'
 if (getCurrentServer("icx") == "sejong") {
-  GOVERNANCE_ADDRESS = 'cx0000000000000000000000000000000000000001'
+  GOVERNANCE_ADDRESS = 'cx0000000000000000000000000000000000000000'
 }
 
 let walletApi, trackerApi

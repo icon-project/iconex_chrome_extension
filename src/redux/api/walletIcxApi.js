207ab54b13a8b004ebfab947740d05ed5ceb6654
Joris Vial <vialjoris@hotmail.com>
2021-08-24 17:09:18 +0900

    Select governance score address and debug api endpoint according to the selected network

--- src/redux/api/walletIcxApi.js
+++ src/redux/api/walletIcxApi.js
@@ -4,8 +4,12 @@ import { signRawTx, customValueToTokenValue, makeIcxRawTx, checkHxPrefix, check0
 import moment from 'moment';
 import BigNumber from 'bignumber.js';
 import { ICX_WALLET_SERVER, ICX_TRACKER_SERVER } from 'constants/config.js';
+import { getCurrentServer } from 'constants/config.js'
 
-const GOVERNANCE_ADDRESS = 'cx0000000000000000000000000000000000000001'
+let GOVERNANCE_ADDRESS = 'cx0000000000000000000000000000000000000000'
+if (getCurrentServer("icx") == "sejong") {
+  GOVERNANCE_ADDRESS = 'cx0000000000000000000000000000000000000001'
+}
 
 let walletApi, trackerApi
 
@@ -342,7 +346,11 @@ export function debug_estimateStepApi(params) {
       //params: rawTx
     }
 
-    walletApi.post(`/api/debug/v3`, JSON.stringify(param))
+    let debugApiUrl = `/api/debug/v3`;
+    if (getCurrentServer("icx") == "sejong") {
+      debugApiUrl = `/api/v3d/`;
+    }
+    walletApi.post(debugApiUrl, JSON.stringify(param))
       .then(res => {
         if (res.data.result) {
           resolve(res.data.result);

ecad4e88f463c083699e1f86bbe5f1ff4b98f239
Joris Vial <joris@iconloop.com>
2021-11-01 15:57:25 +0900

    Prepare ICON2 mainnet launch

--- src/constants/icxContract.js
+++ src/constants/icxContract.js
@@ -1,14 +1,8 @@
-import { getCurrentServer } from 'constants/config.js'
-
 let icxContractData = [
   {
     "address": "cx0000000000000000000000000000000000000001",
     "name": "GOV"
   }
 ];
-
-if (getCurrentServer('icx') == 'sejong') {
-  icxContractData[0].address = "cx0000000000000000000000000000000000000000";
-}
 
 export const icxContract = icxContractData
